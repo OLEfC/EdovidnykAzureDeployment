515965bb1ed07060245c9670531d1edb7f1c5ee0
OLEfC <94687864+OLEfC@users.noreply.github.com>
2023-12-02 11:48:41 +0200

    Update settings.py

--- edovidnyk/settings.py
+++ edovidnyk/settings.py
@@ -134,6 +134,10 @@ USE_TZ = True
 # settings.py
 
 STATIC_URL = '/static/'
+# settings.py
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
