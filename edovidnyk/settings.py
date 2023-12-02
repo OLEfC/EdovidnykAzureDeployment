b2350d97cff3d3e472ea99327a0b74c1aa860bbd
OLEfC <94687864+OLEfC@users.noreply.github.com>
2023-12-02 11:52:37 +0200

    Update settings.py

--- edovidnyk/settings.py
+++ edovidnyk/settings.py
@@ -85,16 +85,18 @@ WSGI_APPLICATION = 'edovidnyk.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 # settings.py
+# settings.py
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'edovidnyk',
-        'USER': 'root',
-        'PASSWORD': '9415',
-        'HOST': 'localhost',  # Адреса сервера баз даних (зазвичай localhost)
+        'USER': 'edovidnyk',
+        'PASSWORD': 'Coursework@1',
+        'HOST': 'edovidnyk.mysql.database.azure.com',  # Адреса сервера баз даних (зазвичай localhost)
         'PORT': '3306',       # Порт сервера баз даних (зазвичай 3306)
     }
 }
+
 
 
 
