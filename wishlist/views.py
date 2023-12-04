17299bb489c5950b3848d83a384d65b5fb6cb8c8
OLEfC <94687864+OLEfC@users.noreply.github.com>
2023-12-04 14:42:37 +0200

    Update views.py

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.views.decorators.http import require_POST
+from django.views.decorators.csrf import csrf_exempt
 
 from django.db import connection
 
